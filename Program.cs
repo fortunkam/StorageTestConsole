525d76c3c4dffb76bba6cfd817a3eddf3edd0ccf
Matthew Fortunka <mafortun@microsoft.com>
2020-01-15 09:56:09 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+
+namespace StorageTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connection String?");
+            var connectionString = Console.ReadLine();
+
+            try
+            {
+                var storageAccount = CloudStorageAccount.Parse(connectionString);
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var containers = blobClient.ListContainers();
+                foreach (var item in containers)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
